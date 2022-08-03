881184573e25ea3a62bb9e004953dbcaa09a4abe
walterxie <walter@cs.auckland.ac.nz>
2022-08-04 10:49:53 +1200

    add script to run all h5n1

---
+++ scripts/runAllH5N1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# cd ~/WorkSpace/linguaPhylo.github.io/tutorials/runs/h5n1
+# run BEAST
+/Applications/BEAST\ 2.6.7/bin/beast -beagle_SSE -overwrite h5n1.xml > out.txt
+
+# run TreeAnnotator
+/Applications/BEAST\ 2.6.7/bin/treeannotator -heights mean -burnin 10 h5n1_with_trait.trees h5n1_with_trait.tree
+
+# run StateTransitionCounter
+/Applications/BEAST\ 2.6.7/bin/applauncher StateTransitionCounter -burnin 10 -in h5n1_with_trait.trees -tag location -out stc.out
+
+# plot
+Rscript ~/WorkSpace/linguaPhylo.github.io/tutorials/discrete-phylogeography/plotTransitionsHunan.R
+
