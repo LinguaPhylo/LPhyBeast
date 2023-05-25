bc4979db9f731561a62ecc6c437031828c000245
Walter <walterxie@users.noreply.github.com>
2023-05-25 15:08:32 +1200

    update dependencies #108 #110

--- lphybeast/build.gradle.kts
+++ lphybeast/build.gradle.kts
@@ -33,7 +33,7 @@ val lblibs by configurations.creating {
 
 // TODO 3 versions: here, LPhyBeastCMD, version.xml
 // version has to be manually adjusted to keep same between version.xml and here
-version = "1.0.0-SNAPSHOT"//-SNAPSHOT
+version = "1.0.1-SNAPSHOT"//-SNAPSHOT
 
 
 // if the project dependencies ues impl, then impl(proj(..)) will only have source code,
@@ -49,7 +49,7 @@ dependencies {
      * such as "io.github.linguaphylo:lphy:1.2.0!!".
      * https://docs.gradle.org/current/userguide/rich_versions.html#sec:strict-version
      */
-    api("io.github.linguaphylo:lphy:1.4.1-SNAPSHOT") //-SNAPSHOT
+    api("io.github.linguaphylo:lphy:1.4.2-SNAPSHOT") //-SNAPSHOT
 
     // all released beast 2 libs
     // TODO beast2 jar contains Apache math. Be aware of version conflict to LPhy dependency.
