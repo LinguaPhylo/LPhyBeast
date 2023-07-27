cabc4bf290fafb3a3a014ebf61c1b6e49920b2ff
Walter <walterxie@users.noreply.github.com>
2023-07-27 15:18:07 +1200

    prepare release 1.0.2 #111

--- lphybeast/build.gradle.kts
+++ lphybeast/build.gradle.kts
@@ -33,7 +33,7 @@ val lblibs by configurations.creating {
 
 // TODO 3 versions: here, LPhyBeastCMD, version.xml
 // version has to be manually adjusted to keep same between version.xml and here
-version = "1.0.1"//-SNAPSHOT
+version = "1.0.2"//-SNAPSHOT
 
 
 // if the project dependencies ues impl, then impl(proj(..)) will only have source code,
@@ -49,6 +49,7 @@ dependencies {
      * such as "io.github.linguaphylo:lphy:1.2.0!!".
      * https://docs.gradle.org/current/userguide/rich_versions.html#sec:strict-version
      */
+    // no API change in 1.4.4
     api("io.github.linguaphylo:lphy:1.4.3") //-SNAPSHOT
 
     // all released beast 2 libs
--- lphybeast/src/main/java/lphybeast/LPhyBeastCMD.java
+++ lphybeast/src/main/java/lphybeast/LPhyBeastCMD.java
@@ -19,7 +19,7 @@ import java.util.concurrent.Callable;
                 "OS: ${os.name} ${os.version} ${os.arch}"})
 public class LPhyBeastCMD implements Callable<Integer> {
 
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
 
     @Parameters(paramLabel = "LPhy_scripts", description = "File of the LPhy model specification. " +
             "If it is a relative path, then concatenate 'user.dir' to the front of the path. " +
