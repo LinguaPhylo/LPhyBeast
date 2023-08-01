81b837b051eff0d020890b3ede18aac26cd7d471
Walter <walterxie@users.noreply.github.com>
2023-08-01 12:27:55 +1200

    correct lphybeast.bat #83 #111

--- lphybeast/bin/lphybeast.bat
+++ lphybeast/bin/lphybeast.bat
@@ -43,13 +43,13 @@ echo set the BEAST_LIB to "%BEAST_LIB%"
 
 if exist "%BEAST%\jre" (
   @rem Use Zulu JRE in the BEAST directory if it exists
-  set "JAVA=%BEAST%\jre\bin\java"
+  set JAVA=%BEAST%\jre\bin\java
 ) else if defined JAVA_HOME (
   @rem Else use %JAVA_HOME%
-  set "JAVA=%JAVA_HOME%\bin\java"
+  set JAVA=%JAVA_HOME%\bin\java
 ) else (
   @rem Else use java from the PATH
-  set "JAVA=java"
+  set JAVA=java
 )
 
 @rem Print Java version
@@ -60,13 +60,13 @@ if not defined LPHY_LIB (
   @rem If %LPHY_LIB% does not exist, detect the OS
   if /I not x%OS:Windows=%==x%OS% (
     echo Guessing LPHY_LIB on Windows ...
-    set "OS_PREFIX=C:\Program Files"
+    set OS_PREFIX="C:\Program Files"
 
     dir /b "%OS_PREFIX%\lphy*studio-1*" > nul
     if errorlevel 1 (
-        set "OS_PREFIX_x86=C:\Program Files (x86)"
+        set OS_PREFIX_x86="C:\Program Files (x86)"
         echo Cannot find the lphy installation in %OS_PREFIX%, try %OS_PREFIX_x86%.
-        set "OS_PREFIX=%OS_PREFIX_x86%"
+        set OS_PREFIX=%OS_PREFIX_x86%
     )
 
   ) else (
@@ -77,7 +77,7 @@ if not defined LPHY_LIB (
   )
 
   @rem If multiple LPhy installations are found, ensure to get the latest version, usually the last line
-  for /f "delims=" %%i in ('dir /b /ad "%OS_PREFIX%\lphy*studio-1*"') do set "LPHY_DIR=%OS_PREFIX%\%%i"
+  for /f "delims=" %i in ('dir /b /ad %OS_PREFIX%\"lphy*studio-1*"') do set "LPHY_DIR=%OS_PREFIX%\%i"
   @rem Guess LPHY_LIB
   set "LPHY_LIB=%LPHY_DIR%\lib"
 )
