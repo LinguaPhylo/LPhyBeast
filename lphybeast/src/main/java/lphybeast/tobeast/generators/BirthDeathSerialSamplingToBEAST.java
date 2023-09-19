c8f112d0eda08a2380f65fe3618c49907b1f3b7f
walterxie <walter@cs.auckland.ac.nz>
2023-09-19 16:36:23 +1200

    BirthDeathSerialSampling error #118

--- lphybeast/src/main/java/lphybeast/tobeast/generators/BirthDeathSerialSamplingToBEAST.java
+++ lphybeast/src/main/java/lphybeast/tobeast/generators/BirthDeathSerialSamplingToBEAST.java
@@ -2,10 +2,9 @@ package lphybeast.tobeast.generators;
 
 import bdtree.likelihood.BirthDeathSequentialSampling;
 import beast.base.core.BEASTInterface;
-import beast.base.evolution.tree.MRCAPrior;
-import beast.base.evolution.tree.Tree;
-import beast.base.inference.distribution.Prior;
 import lphy.evolution.birthdeath.BirthDeathSerialSamplingTree;
+import lphy.graphicalModel.Value;
+import lphy.graphicalModel.ValueUtils;
 import lphybeast.BEASTContext;
 import lphybeast.GeneratorToBEAST;
 
@@ -23,25 +22,44 @@ public class BirthDeathSerialSamplingToBEAST implements
         beastBDSS.setInputValue("rho", context.getAsRealParameter(generator.getRho()));
         beastBDSS.setInputValue("psi", context.getAsRealParameter(generator.getPsi()));
         beastBDSS.setInputValue("tree", tree);
-        beastBDSS.initAndValidate();
 
-        BEASTInterface beastRootAge = context.getAsRealParameter(generator.getRootAge());
-        BEASTInterface beastRootAgeGenerator = context.getBEASTObject(generator.getRootAge().getGenerator());
+        // 2 scenarios : 1) root age; 2) root age lower and upper bound.
+        // Both cannot use getAsRealParameter which will create scale op.
+//        Input<Double> rootAgeLowerInput; Input<Double> rootAgeUpperInput;
+//        Input<Double> rootAgeInput;
+        Value<Number> rootAgeVal = generator.getRootAge();
+        if (rootAgeVal.getGenerator() != null) {
+            throw new UnsupportedOperationException("The development of specifying lower and upper bounds for the root age is still in progress !");
 
-        if (beastRootAgeGenerator instanceof Prior) {
-            Prior rootAgePrior = (Prior) beastRootAgeGenerator;
+            /*TODO Alexei: what is RootAgeGenerator? I assume you want to assign the lower and upper here
+            // https://github.com/fkmendes/bdtree/blob/master/examples/testing/Shankarappa.xml
+            BEASTInterface beastRootAgeGenerator = context.getBEASTObject(generator.getRootAge().getGenerator());
+//            beastBDSS.setInputValue("lower", lower);
+//            beastBDSS.setInputValue("upper", upper);
 
-            MRCAPrior prior = new MRCAPrior();
-            prior.setInputValue("distr", rootAgePrior.distInput.get());
-            prior.setInputValue("tree", tree);
-            prior.setInputValue("taxonset", ((Tree) tree).getTaxonset());
-            prior.initAndValidate();
-            context.addBEASTObject(prior, generator.getRootAge().getGenerator());
-            context.removeBEASTObject(beastRootAge);
-            context.removeBEASTObject(beastRootAgeGenerator);
+            if (beastRootAgeGenerator instanceof Prior) {
+                Prior rootAgePrior = (Prior) beastRootAgeGenerator;
+
+                MRCAPrior prior = new MRCAPrior();
+                prior.setInputValue("distr", rootAgePrior.distInput.get());
+                prior.setInputValue("tree", tree);
+                prior.setInputValue("taxonset", ((Tree) tree).getTaxonset());
+                prior.initAndValidate();
+                context.addBEASTObject(prior, generator.getRootAge().getGenerator());
+//                context.removeBEASTObject(beastRootAge);
+                context.removeBEASTObject(beastRootAgeGenerator);
+            } else {
+                throw new RuntimeException("Can't map BirthDeathSamplingTree.rootAge prior to tree in BEAST conversion.");
+            }*/
+
         } else {
-            throw new RuntimeException("Can't map BirthDeathSamplingTree.rootAge prior to tree in BEAST conversion.");
+            // https://github.com/fkmendes/bdtree/blob/master/examples/testing/BDSSLikelihood.xml
+            Double rootAge = ValueUtils.doubleValue(rootAgeVal);
+            beastBDSS.setInputValue("rootAge", rootAge);
+            //TODO how to remove tree root op: spec="ScaleOperator" rootOnly="true" tree="@psi"
         }
+
+        beastBDSS.initAndValidate();
 
         return beastBDSS;
     }
