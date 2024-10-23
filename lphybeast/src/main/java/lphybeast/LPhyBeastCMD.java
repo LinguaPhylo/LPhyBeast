1e9b0b3e66ef37a8c8cd5d848eaf21ce18c85282
Walter <walterxie@users.noreply.github.com>
2024-10-24 10:02:28 +1300

    Prepare release 1.2.1 #159

--- lphybeast/src/main/java/lphybeast/LPhyBeastCMD.java
+++ lphybeast/src/main/java/lphybeast/LPhyBeastCMD.java
@@ -19,7 +19,7 @@ import java.util.concurrent.Callable;
                 "OS: ${os.name} ${os.version} ${os.arch}"})
 public class LPhyBeastCMD implements Callable<Integer> {
 
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.2.1";
 
     @Parameters(paramLabel = "LPhy_scripts", description = "File of the LPhy model specification. " +
             "If it is a relative path, then concatenate 'user.dir' to the front of the path. " +
