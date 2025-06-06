7118645b15f7b904cb29ba5b8605cce590bc7b7e
EvaLiyt <liyutong0617@gmail.com>
2025-06-06 15:12:37 +1200

    exclude informativeSites generator

--- lphybeast/src/main/java/lphybeast/spi/LPhyBEASTExtImpl.java
+++ lphybeast/src/main/java/lphybeast/spi/LPhyBEASTExtImpl.java
@@ -132,7 +132,7 @@ public class LPhyBEASTExtImpl implements LPhyBEASTExt {
                 NTaxaFunction.class, NCharFunction.class, TaxaFunction.class, CreateTaxa.class,
                 SpeciesTaxa.class, TaxaAgesFromFunction.class, RemoveTaxa.class,
                 CopySites.class, MissingSites.class, SelectSitesByMissingFraction.class, InvariableSites.class,
-                VariableSites.class, Distance.class,
+                VariableSites.class, Distance.class, InformativeSites.class,
                 DiscretizedGamma.class,
                 // Tree
                 NodeCount.class, ExtractTrait.class, ExtantTree.class,
