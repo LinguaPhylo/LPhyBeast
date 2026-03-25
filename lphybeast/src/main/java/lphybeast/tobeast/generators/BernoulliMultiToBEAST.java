c7889f394b97e971daee987728f8e26a0b359cca
alexeid <alexei.drummond@gmail.com>
2026-03-25 16:30:29 +1300

    Fix BernoulliMultiToBEAST: wrap scalar p as 1-element RealVectorParam
    BernoulliDistribution.p expects RealVectorParam but LPhy p=0.5 creates
    a RealScalarParam. Wrap as 1-element vector for IID broadcast.
    
    H5N1 test now fails deeper in VectorizedFunctionToBEAST (DiscretizeGamma
    vectorization), not BernoulliMulti.

--- lphybeast/src/main/java/lphybeast/tobeast/generators/BernoulliMultiToBEAST.java
+++ lphybeast/src/main/java/lphybeast/tobeast/generators/BernoulliMultiToBEAST.java
@@ -1,6 +1,9 @@
 package lphybeast.tobeast.generators;
 
 import beast.base.core.BEASTInterface;
+import beast.base.spec.domain.UnitInterval;
+import beast.base.spec.inference.parameter.RealVectorParam;
+import beast.base.spec.type.RealScalar;
 import beastlabs.math.distributions.BernoulliDistribution;
 import lphy.base.distribution.BernoulliMulti;
 import lphybeast.BEASTContext;
@@ -12,8 +15,16 @@ public class BernoulliMultiToBEAST implements GeneratorToBEAST<BernoulliMulti, B
     @Override
     public BernoulliDistribution generatorToBEAST(BernoulliMulti generator, BEASTInterface value, BEASTContext context) {
 
+        // BernoulliDistribution.p expects RealVectorParam; wrap scalar p as 1-element vector
+        BEASTInterface pObj = context.getBEASTObject(generator.getP());
+        if (pObj instanceof RealScalar<?> scalar) {
+            RealVectorParam<UnitInterval> pVec = new RealVectorParam<>(new double[]{scalar.get()}, UnitInterval.INSTANCE);
+            pVec.setID(((BEASTInterface) scalar).getID());
+            pObj = pVec;
+        }
+
         BernoulliDistribution bernoulliDistribution = new BernoulliDistribution();
-        bernoulliDistribution.setInputValue("p", context.getBEASTObject(generator.getP()));
+        bernoulliDistribution.setInputValue("p", pObj);
         bernoulliDistribution.setInputValue("parameter", value);
         bernoulliDistribution.setInputValue(generator.minSuccessesParamName, context.getBEASTObject(generator.getMinSuccesses()));
         bernoulliDistribution.initAndValidate();
